5032c1cb32349951c76ba58eb59b1789fe728bdd
ophiophagus_hannah <hannahavgust@gmail.com>
2020-10-16 22:11:38 -0500

    fixed dumbery

--- nav.php
+++
@@ -1,5 +0,0 @@
-<nav class='nav'>
-    <ul >
-        <li ><a href="#sl-1">Test</a></li>
-    </ul>
-</nav>
--- slides.php
+++
@@ -1,6 +0,0 @@
-<div class="slides">
-    <section data-transition="slide" id="sl-1">
-        <h2>Test Slide</h2>
-        <p>Links and Content</p>
-    </section>
-</div>
